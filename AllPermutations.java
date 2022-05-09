95c9701199404eebd127855daf5fa4e8cfcffe11
Alex Oh <oalex@knights.ucf.edu>
2022-05-09 10:44:04 -0400

    String Permutations Backtracking Solution

---
+++ AllPermutations.java
@@ -0,0 +1,23 @@
+public class AllPermutations {
+    public static void main(String[] args){
+        char []  array = {'a', 'b', 'c','d'};
+        printAllPermutations(array, 4);
+    }
+    public static void printAllPermutations(char[] array, int length){
+        if(length == 1){
+            System.out.println(array);
+        }
+        for(int i = 0; i < length; i++){
+            swap(array, i, length-1);
+            printAllPermutations(array, length-1);
+            swap(array, i, length-1);
+        }
+    }
+    
+
+    public static void swap(char [] array, int i, int j){
+        char c = array[i];
+        array[i] = array[j];
+        array[j] = c;
+    }
+}
