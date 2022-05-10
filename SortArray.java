8d947921523713c0ba2b0d9899c238c0466d168e
Alex Oh <oalex@knights.ucf.edu>
2022-05-10 07:11:28 -0400

    Sort an Array Solution

---
+++ SortArray.java
@@ -0,0 +1,16 @@
+public class SortArray {
+    public static void main(String [] args){
+    }
+    public static void sortArray(int [] array, int n){
+        if(n == 1) return;
+
+        for(int i = 0; i < n-1; i++){
+            if(array[i] > array[i+1]){
+                int temp = array[i];
+                array[i] = array[i+1];
+                array[i+1] = temp;
+            }
+        }
+        sortArray(array, n - 1);
+    }
+}
