c134af967e0439cfff3ed3a5dd6a19f64925c158
Alex Oh <oalex@knights.ucf.edu>
2022-05-13 07:08:29 -0400

    Kadane's Algorithm

--- AceTheJavaCodingInterview/Data Structures/Arrays/MaximumSumSubarray.java
+++ AceTheJavaCodingInterview/Data Structures/Arrays/MaximumSumSubarray.java
@@ -31,4 +31,21 @@ public class MaximumSumSubarray {
         }
         return max;
     }
+    public static int findMaxSumSubArray3(int [] arr){
+        int currMax = arr[0];
+        int globalMax = arr[0];
+
+        for(int i = 0; i < arr.length; i++){
+            if(currMax < 0){
+                currMax = arr[i];
+            }else{
+                currMax = arr[i] + currMax;
+            }
+
+            if(globalMax < currMax){
+                globalMax = currMax;
+            }
+        }
+        return globalMax;
+    }
 }
