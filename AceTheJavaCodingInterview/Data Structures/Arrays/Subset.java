4f144a41c66e183a92451bd325e338b054976b35
Alex Oh <oalex@knights.ucf.edu>
2022-05-14 12:44:40 -0400

    Subset

---
+++ AceTheJavaCodingInterview/Data Structures/Arrays/Subset.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Subset {
+    public static void main(String [] args){
+        
+    }
+
+    public static List<List<Integer>> findSubsets(int [] nums){
+        List<List<Integer>> results = new ArrayList<>();
+        //add an empty set to the list
+        results.add(new ArrayList<>());
+
+        for(int i = 0; i < nums.length; i++){
+            int currentNumber = nums[i];
+            int listSize = results.size();
+
+            for(int j = 0; j < listSize; j++){
+                List<Integer> curr = new ArrayList<>(results.get(j));
+                curr.add(currentNumber);
+                results.add(curr);
+            }
+        }
+        return results;
+    }
+}
