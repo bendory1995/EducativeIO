6eb209bc466fdd37457025a3d7da494bb45592fc
Alex Oh <oalex@knights.ucf.edu>
2022-05-17 08:58:49 -0400

    Subarrays Product Less Than K LeetCode

---
+++ AceTheJavaCodingInterview/Data Structures/Arrays/SubarrayProductLessThanK.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class SubarrayProductLessThanK {
+    public static void main(String [] args){
+
+    }
+    public static int findSubarrays(int [] arr, int target){
+        int count = 0;
+        int left = 0;
+        int product = 1;
+        for(int right = 0; right < arr.length; right++){
+            product = product * arr[right];
+            while(product > target) product = product / arr[left++];
+            count = count + right - left + 1;
+        }
+        return count;
+    }
+    
+}
